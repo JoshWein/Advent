0a24bf5345adb71de4a6deaec0bde3172734709b
Josh Wein <jdog90000@yahoo.com>
2015-12-04 01:02:47 -0500

    Day 4 Solution: The Ideal Stocking Stuffer
    Cleaned up code.

--- src/advent4/Advent4.java
+++ src/advent4/Advent4.java
@@ -1,34 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package advent4;
 
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
  *
- * @author Josh
+ * @author Josh Wein
  */
 public class Advent4 {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+        MessageDigest md = java.security.MessageDigest.getInstance("MD5");
         int i =0;
         StringBuilder sb = new StringBuilder();
         sb.append("11111111");
-        String test = "11111111111";
+        String test;
         byte[] array;
         while(!sb.substring(0, 6).equals("000000")) {
-            sb = new StringBuilder();
+            sb.setLength(0);
             test = "bgvyzdsv".concat(Integer.toString(i++));
             array = md.digest(test.getBytes());
             for (int j = 0; j < array.length; ++j) {
-                sb.append(Integer.toHexString((array[j] & 0xFF) | 0x100).substring(1, 3));
+                sb.append(Integer.toHexString((array[j] & 0xFF) | 0x100).substring(1, 3)); 
             }
         }
         System.out.println(sb);
