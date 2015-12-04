4ba1cdb99ebefa67314fa85809f2d5e11d00b008
Josh Wein <jdog90000@yahoo.com>
2015-12-04 13:34:37 -0500

    Day 1 Solution: Not Quite Lisp
    Cleaned up, optimized and commented day 1 and 4

--- src/advent4/Advent4.java
+++ src/advent4/Advent4.java
@@ -11,15 +11,17 @@ import java.security.NoSuchAlgorithmException;
 public class Advent4 {
     public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         MessageDigest md = MessageDigest.getInstance("MD5");
-        int i =0;
+        int i = 0;
         byte[] array;
-        while(true) {
-            array = md.digest("bgvyzdsv".concat(Integer.toString(i++)).getBytes()); // Put input here
-            if(array[0] == 0 && array[1] == 0 && (array[2]>> 4 & 0xf) == 0) {
-                if(array[2] == 0) // Comment out to do part 1.
+        long a = System.currentTimeMillis();
+        while(true) {   // Concat key with counter starting from 0, and hash until we get the right result
+            array = md.digest("bgvyzdsv".concat(Integer.toString(i++)).getBytes()); // Put input here, store the md5 hash bytes in an array
+            if(array[0] == 0 && array[1] == 0 && (array[2]>> 4 & 0xf) == 0) { // Test first two bytes and first two bits of third byte
+                if(array[2] == 0) // Comment out to do part 1, test whole third byte instead of just the first half
                     break;
             }
         }
+        System.out.println("Total time: " + (System.currentTimeMillis() - a) + "ms"); // Time to complete
         System.out.println("Lowest value needed: " + (i-1));
     }
 }
